7510088d687d561a51daaabc62143a7e6d226dea
James Burnside <jcburnside97@gmail.com>
2017-06-09 16:49:53 -0400

    test

--- robots.js
+++ robots.js
@@ -3,7 +3,7 @@ function load(){
 	var xhttp=new XMLHttpRequest;
 	xhttp.onreadystatechange=function(){
 		if(this.readyState==4&&this.status==200){
-			var TEST=this
+			xml=new XMLFile(this)
 			console.log("READY!!!!!");
 		}
 	}
